03f66992d5655912c55df33cb160be0609dec947
Lourdes Velo Suarez <lourdesvelo@gmail.com>
2021-03-10 14:02:40 +0100

    chais pas

--- scripts/dada2/02_learnErrorRates.R
+++ scripts/dada2/02_learnErrorRates.R
@@ -3,8 +3,9 @@ suppressPackageStartupMessages(library(dada2))
 suppressPackageStartupMessages(library(ggplot2))
 sink(snakemake@log[[1]])
 
-
+set.seed(100)
 errF <- learnErrors(snakemake@input[['r1']], nbases=snakemake@config[["learn_nbases"]], multithread=snakemake@threads,randomize = TRUE)
+set.seed(100)
 errR <- learnErrors(snakemake@input[['r2']], nbases=snakemake@config[["learn_nbases"]], multithread=snakemake@threads,randomize = TRUE)
 
 save(errF,file=snakemake@output[['err_r1']])
